46226c54022710c4baf48133b7fc50cc7e386648
Pablo Cortés <cortesoltra@gmail.com>
2025-04-06 02:15:01 +0200

    Build Android app on Github Actions for download

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
@@ -6,6 +9,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = project.rootProject.file("key.properties") // Lee desde /android
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -23,6 +32,18 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            // Asumimos que key.properties existirá cuando se use esta config para release
+            // La Action se encarga de crearlo siempre.
+            storeFile = file(keystoreProperties.getProperty("storeFile"))
+            storePassword = keystoreProperties.getProperty("storePassword")
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+        }
+        // 'debug' está disponible por defecto
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.hcc_app"
@@ -35,10 +56,11 @@ android {
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") { // Usamos getByName para configurar el tipo "release"
+            // Asegúrate de que la línea para aplicar la firma sea así:
+            signingConfig = signingConfigs.getByName("release") // Usar getByName aquí también
+
+            // ... otras configuraciones de release ...
         }
     }
 }
